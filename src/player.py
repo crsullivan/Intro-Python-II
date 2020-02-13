db8ed5e710a6236aff74372022c9170c4227f22a
Connor Sullivan <crsullivan@protonmail.com>
2020-02-13 10:50:50 -0700

    basic player class made, moving to room.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, place):
+        self.name = name
+        self.place = place
+
+    def __str__(self):
+        print(f"{self.name}\n{self.place}")
